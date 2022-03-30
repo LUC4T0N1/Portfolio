4507a7c19d6ebfb491208f2dafa4cb55e6752149
LUC4T0N1 <lucas_moniz@hotmail.com>
2022-03-30 18:07:38 -0300

    first commit

---
+++ src/interfaces/intro/Intro.js
@@ -0,0 +1,28 @@
+import './Intro.css';
+
+function Intro() {
+  return (
+    <div className="intro">
+     <div className='intro-left'>
+        <div className='intro-left-wrapper'>
+          <h2 className='intro-intro'>Hello, my name is</h2>
+          <h1 className='intro-name'>Lucas Moniz de Arruda</h1>
+          <div className='intro-title'>
+            <div className='intro-title-wrapper'>
+             <div className='intro-title-item'>Full-Stack Web Developer</div>
+             <div className='intro-title-item'>Computer Cience Bacharel</div>
+            </div>
+          </div>
+          <div className='intro-description'>
+            I develop full web softwares from Front-End to Back-End using React, Redux, Java, Quarkus and Mysql
+          </div>
+        </div>
+     </div>
+     <div className='intro-right'>
+        <div className='intro-background'></div>
+     </div>
+    </div>
+  );
+}
+
+export default Intro;
