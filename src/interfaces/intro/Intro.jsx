7828329274864542b63706853f763db745d8b2a9
LUC4T0N1 <lucas_moniz@hotmail.com>
2022-06-19 17:55:09 -0300

    ajsutes cores

--- src/interfaces/intro/Intro.jsx
+++ src/interfaces/intro/Intro.jsx
@@ -1,11 +1,14 @@
 import './Intro.css';
 import {useTranslation} from 'react-i18next';
+import { ThemeContext } from "../../infrastructure/context";
+import { useContext } from "react";
 
 function Intro() {
   const {t} = useTranslation()
+  const theme =  useContext(ThemeContext);
   return (
     <div className="intro">
-          <div className='intro-background'></div>
+          <div className='intro-background' style={{ background: theme.state.darkMode ?   "linear-gradient(to left, rgb(12, 9, 10), rgb(190, 31, 79))" : "linear-gradient(to left, rgb(114, 24, 51), rgb(252, 192, 213))" }}></div>
      <div className='intro-left'>
         <div className='intro-left-wrapper'>
           <h2 className='intro-intro'>{t('hello')}</h2>
